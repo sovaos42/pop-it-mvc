1472a8b5b35bbe6ee9f93333fe2415c119728480
sovaos42 <sovaos42@gmail.com>
2023-04-03 11:36:40 +0700

    Работа с сессиями и редиректами

--- app/Model/User.php
+++ app/Model/User.php
@@ -4,6 +4,7 @@ namespace Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Src\Auth\IdentityInterface;
 
 class User extends Model
 {
@@ -23,4 +24,23 @@ class User extends Model
             $user->save();
         });
     }
+    //Выборка пользователя по первичному ключу
+    public function findIdentity(int $id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+    //Возврат первичного ключа
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    //Возврат аутентифицированного пользователя
+    public function attemptIdentity(array $credentials)
+    {
+        return self::where(['login' => $credentials['login'],
+            'password' => md5($credentials['password'])])->first();
+    }
+
 }
